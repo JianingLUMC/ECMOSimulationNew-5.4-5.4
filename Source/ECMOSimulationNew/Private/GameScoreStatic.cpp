3a23bd3a13f26d9a5bee2a9f6843d5255079cb32
JianingLUMC <j.zhao@lumc.nl>
2025-04-11 11:54:19 +0200

    trials on saving score

--- Source/ECMOSimulationNew/Private/GameScoreStatic.cpp
+++ Source/ECMOSimulationNew/Private/GameScoreStatic.cpp
@@ -7,7 +7,7 @@
 
 bool UGameScoreStatic::AppendStringsToCSV(const FString& FilePath, const FString& String1, const FString& String2, const FString& String3, const FString& String4)
 {
-    FString RowData = FString::Printf(TEXT("\"%s\",\"%s\",\"%s\",\"%s\"\n"), *String1, *String2, *String3, *String4);
+    FString RowData = FString::Printf(TEXT("\"%s\",\"%s\",\"%s\",\"%s\"\n"), *String1, *String2, *String3, *String4, LINE_TERMINATOR);
 
     return FFileHelper::SaveStringToFile(RowData, *FilePath, FFileHelper::EEncodingOptions::AutoDetect, &IFileManager::Get(), FILEWRITE_Append);
 }
